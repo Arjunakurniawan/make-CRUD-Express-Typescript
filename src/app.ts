fdeb2660dac1a4ff566639b524a29e048f02eea0
ArjunaKurniawan <arjunakurniawan686@gmail.com>
2025-01-05 18:30:26 +0700

    update: refactor code

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,61 @@
 import express from "express";
-import { createUser, getUsers } from "./controllers/userControllers";
 
 const app = express();
-
 const PORT = 3000;
+
+import type { Request, Response } from "express";
+
+type User = {
+  id: number;
+  name: string;
+};
+
+const users: User[] = [
+  { id: 1, name: "jhon" },
+  { id: 2, name: "jane" },
+  { id: 3, name: "alice" },
+];
 
 app.use(express.json());
 
-app.get("/users", getUsers);
+app.get("/users", (req: Request, res: Response) => {
+  res.json(users);
+});
 
-app.post("/users", createUser);
+app.post("/users", (req: Request, res: Response) => {
+  const { name } = req.body;
+  if (name === undefined) {
+    res.status(404).json({ message: "user not found" });
+  }
+  const id = users.length + 1;
+  const newUser = { id, name };
+  users.push(newUser);
+  res.status(201).json({ message: "user added successfully" });
+});
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.delete("/users/:id", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+  const index = users.findIndex((user) => user.id === id);
+  if (index === -1) {
+    res.status(404).json({ message: "user not found" });
+  }
+  users.splice(index, 1);
+  res.status(200).json({ message: "user deleted successfully" });
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+  const index = users.findIndex((user) => user.id === id);
+  if (index === -1) {
+    res.status(404).json({ message: "user not found" });
+  }
+  const { name } = req.body;
+  users[index].name = name;
+  res.json({ message: "user updated successfully" });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(404).json({ message: "user not found" });
 });
 
 app.listen(PORT, () => {
